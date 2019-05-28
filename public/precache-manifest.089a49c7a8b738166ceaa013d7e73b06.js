b5fbf1460fda37c1879970255766002ed619673d
Chloe <chloe@Chloes-MacBook-Air.local>
2019-05-28 11:18:37 +0200

    heroku update

---
+++ public/precache-manifest.089a49c7a8b738166ceaa013d7e73b06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b75d0fcfa83456d6709",
+    "url": "/static/css/main.0af13ec5.chunk.css"
+  },
+  {
+    "revision": "0b75d0fcfa83456d6709",
+    "url": "/static/js/main.0b75d0fc.chunk.js"
+  },
+  {
+    "revision": "59a0ee1179d32c90e402",
+    "url": "/static/js/1.59a0ee11.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f77ec45f8941c4df40c63de74d58440",
+    "url": "/static/media/logo_skillsamp_L.8f77ec45.png"
+  },
+  {
+    "revision": "ef76076123d7ffe4ae12c1081e2aa3df",
+    "url": "/static/media/profile_placeholder.ef760761.png"
+  },
+  {
+    "revision": "51bb600b8cc2439952ed9301adaa8253",
+    "url": "/static/media/bg_profile.51bb600b.png"
+  },
+  {
+    "revision": "51431bffc2ca529011b9653a24858a58",
+    "url": "/index.html"
+  }
+];
