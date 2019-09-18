933f178e0f7fd5d08935c73f3b09ac18baf88d9d
Chloé <chloe.leteinturier@gmail.com>
2019-09-18 18:59:32 +0200

    cors modified

--- app.js
+++ app.js
@@ -36,13 +36,13 @@ app.use(cors({
   credentials: true,
   origin: [process.env.PUBLIC_DOMAIN, 'http://skillsamp.herokuapp.com/']
 }));
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://skillsamp.herokuapp.com/');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 // SESSION MIDDLEWARE
 app.use(session({
