6c5268d698c2104bd1f3eaa7cab491d399eca118
Chloe <chloe@Chloes-MacBook-Air.local>
2019-09-18 19:17:19 +0200

    cors modified

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors({
   origin: [process.env.PUBLIC_DOMAIN, 'http://skillsamp.herokuapp.com/']
 }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://skillsamp.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
