df73ed8acc63c71eeea1ee4c76e2815eb848b1c7
Chloe <chloe@Chloes-MacBook-Air.local>
2019-12-09 15:42:45 +0100

    build

---
+++ public/precache-manifest.7ed8b31d862ab1f862822cc1a15e6385.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f0c695fd8e19ef5adb187b47bd187d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf7f9f3ce6837dd13ad",
+    "url": "/static/css/main.d2b9e671.chunk.css"
+  },
+  {
+    "revision": "611f337dffc57dce0bfa",
+    "url": "/static/js/2.1b666c69.chunk.js"
+  },
+  {
+    "revision": "6d0c6aad67b53ae8eed4abce60eb8cfc",
+    "url": "/static/js/2.1b666c69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdf7f9f3ce6837dd13ad",
+    "url": "/static/js/main.0b75a8ed.chunk.js"
+  },
+  {
+    "revision": "ac25e3d92b2b3dc23926",
+    "url": "/static/js/runtime-main.9b23aeff.js"
+  },
+  {
+    "revision": "51bb600b8cc2439952ed9301adaa8253",
+    "url": "/static/media/bg_profile.51bb600b.png"
+  },
+  {
+    "revision": "8f77ec45f8941c4df40c63de74d58440",
+    "url": "/static/media/logo_skillsamp_L.8f77ec45.png"
+  }
+]);
