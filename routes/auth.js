6ae82471ce3769f48968051a7aa0da31886a1f70
Chloe <chloe@Chloes-MacBook-Air.local>
2019-12-10 09:52:25 +0100

    first

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-console.log('holiiii');
-
 const parser = require('./../config/cloufinary');
 
 
